72424e5371b064fa46abb3693769d0b3684893bd
Christian Kaltepoth <christian@kaltepoth.de>
2014-09-12 16:58:42 +0200

    Demo for calendar

---
+++ demo/src/main/java/de/chkal/jsfjs/calendar/CalendarBean.java
@@ -0,0 +1,42 @@
+package de.chkal.jsfjs.calendar;
+
+import java.util.Date;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class CalendarBean {
+
+  private Date date;
+
+  public String action() {
+
+    String message = "Keine Datum ausgewählt";
+
+    if (date != null) {
+      if (date.after(new Date())) {
+        message = "Datum liegt in der Zukunft";
+      } else {
+        message = "Datum liegt in der Vergangenheit";
+      }
+    }
+
+    FacesContext.getCurrentInstance().addMessage(
+        null, new FacesMessage(message));
+    return null;
+
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+}
